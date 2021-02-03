223dbf115aa4e6b7bc38df3f7541bbe2daf02980
lalexander111 <78442790+lalexander111@users.noreply.github.com>
2021-02-03 11:05:44 -0500

    Update addrecipe.php

--- addrecipe.php
+++ addrecipe.php
@@ -34,6 +34,26 @@
                 <div class="block-heading">
                     <h2 class="text-info">Add a Recipe</h2>
                 </div>
+          <?php
+            $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+            if (strpos($url, "error=emptyfields" == true)){
+                echo "<p class='error'>Fill in all fields!</p>";
+                exit();
+            }
+            elseif (strpos($url, "error=toolarge" == true)){
+                echo "<p class='error'>Image file is too large!</p>";
+                exit();
+            }
+            elseif (strpos($url, "error=file_error" == true)){
+                echo "<p class='error'>Unknown file error!</p>";
+                exit();
+            }
+            elseif (strpos($url, "error=toolarge" == true)){
+                echo "<p class='error'>Image extension not supported!</p>";
+                exit();
+            }
+          ?>
                 <form>
                     <label>Image</label><div class="form-group"><input class="form-control2" type="file" name="recipefile"></div>
                     <div class="form-group"><label>Title</label><input class="form-control" type="text" name="recipetitle"></div>
@@ -47,38 +67,6 @@
     <footer class="page-footer dark">
         <div class="container">
             <div class="row">
-                <div class="col-sm-3">
-                    <h5>Get started</h5>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Sign up</a></li>
-                        <li><a href="#">Downloads</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h5>About us</h5>
-                    <ul>
-                        <li><a href="#">Company Information</a></li>
-                        <li><a href="#">Contact us</a></li>
-                        <li><a href="#">Reviews</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h5>Support</h5>
-                    <ul>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Help desk</a></li>
-                        <li><a href="#">Forums</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h5>Legal</h5>
-                    <ul>
-                        <li><a href="#">Terms of Service</a></li>
-                        <li><a href="#">Terms of Use</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                    </ul>
-                </div>
             </div>
         </div>
         <div class="footer-copyright">
