3e3734af9297632ae8bddf2c09d55ce82a88ba69
lynqux <57960864+lynqux@users.noreply.github.com>
2021-02-03 13:40:51 -0500

    fixed some stuff

--- assets/js/recipe_directory.js
+++ assets/js/recipe_directory.js
@@ -1,52 +1,48 @@
 var recipearray = [];
 var arrlength = 0;
 var recipetitle = [];
-
 //get request here 
 function getvals(){
+    /*
+    fetch("https://plantfoodapi.herokuapp.com/recipe/all").then(function(response){
+    return response.json();
+    })
+    .then(function(json){
+        for(r of json){
+            let recipeBox = document.createElement("div");
+            recipeBox.classList.add("indivRecipe");
+            recipeBox.innerHTML = '<h3 id="a">' + r.title + '</h3>';
+            allRecipes.appendChild(recipeBox);
+        }
+    });
+    */
     let url = 'https://plantfoodapi.herokuapp.com/recipe/all';
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
-
+    xhr.send();
     xhr.onload = function (e) {
         if (xhr.readyState == 4) {
             if (xhr.status == 200) { 
-                recipearray.empty(); 
+                recipearray.empty; 
                 recipearray = JSON.parse(xhr.responseText); // parse get array -- need img and title
-                //updaterecipes();
+                updaterecipes();
             }
         }
     }
 }
-
 function pageLoaded(){
     getvals(); //replace with getvals() since updaterecipes already in get req
 }
-
 function updaterecipes(){
     //recipearray = ['<div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image1.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image1.jpg"></a></div>',
     //'<div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image4.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image4.jpg"></a></div>',
     //'<div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image6.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image6.jpg"></a></div>']
-
     let gallery = document.getElementById('recipegallery');
-
-    if (type(recipearray.length / 7) == int){
-        arrlength = recipearray.length / 7;
-    }
-    else{ 
-        arrlength = Math.floor(recipearray.length / 7); 
-    }
-
-    for (let j = 1; j < recipearray.length; j+= 7){
-        recipetitle.push(recipearay[1]);
-    }
-
-
     for (let i = 0; i < recipearray.length; i++){
+        var title = recipearray[i].title;
         let node= document.createElement("div");
         node.className = "col-md-6 col-lg-4 item";
-        node.innerHTML = '<a class="lightbox" href="assets/img/scenery/image1.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image1.jpg"></a>'; //can add clickable att to img
-
+        node.innerHTML = '<center>' + title + '</center>' + '<a class="lightbox" href="assets/img/img' + recipearray[i].id + '.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/img' + recipearray[i].id + '.jpg"></a>'; //can add clickable att to img
         gallery.appendChild(node);
     }
 }
